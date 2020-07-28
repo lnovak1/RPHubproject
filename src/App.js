77b2f54c1166fae8a6626f2773ef73efaf7a1a8c
lnovak1 <lnvk98@gmail.com>
2020-07-28 01:03:14 -0300

    first

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logoflix from './assets/logoflix.png'
+import './App.css';
+
+function App() {
+  return (
+    <img src={logoflix} alt= "NOVAFLIX" />
+  );
+}
+
+export default App;
